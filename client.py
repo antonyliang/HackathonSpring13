28f805a1cf9fa8c2fb304fa2a6c86f2547371285
StanleyY <StanleyY@github.com>
2013-05-03 21:26:35 -0400

    started storing costs

--- client.py
+++ client.py
@@ -1,18 +1,28 @@
 #Team Midas
 import socket
 
+Revenue = 0
 W_cost = 0
 J_cost = 0
 D_cost = 0
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("hackathon.hopto.org", 27832))
-s.send("INIT Midas")
-data = s.recv(1024)
-print data
-s.send("RECD")
-data = s.recv(1024)
-print data
+def init():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("hackathon.hopto.org", 27832))
+    s.send("INIT Midas")
+    data = s.recv(1024)
+    print data
+    s.send("RECD")
+    data = s.recv(1024)
+    print data
+
+def parseCost(data):
+    
+    print "REVENUE "+ Revenue
+    print "WEB " + W_cost
+    print "Java " + J_cost
+    print "Data " + D_cost
+
 s.send("START")
 data = s.recv(1024)
 print data
