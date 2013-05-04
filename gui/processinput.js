57829ebe8fc08df054ee37ee9012beceac4dcaed
Andrew Spano <andrew.spano1@marist.edu>
2013-05-04 06:08:04 -0400

    added to string to Input prototype

--- gui/processinput.js
+++ gui/processinput.js
@@ -1,7 +1,11 @@
 //stores actual file processing stuff
 
-var input = function () {
+var Input = function () {
     this.file=read_file();//stores the file with the information
     this.line = 0; //stores the position in the file
-    this.dat = this.file.split("\n");  //the array of elements split by newline
+    this.stats = this.file.split("\n");  //the array of elements split by newline
+    this.toString() = function () {
+        return "File: " + this.file + " current line position: " + this.line
+        + " split array: " + this.stats;
+    };
 };
