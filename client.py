83c71138a707d1851da1a0cba4aba67fde459e64
StanleyY <StanleyY@github.com>
2013-05-03 21:57:21 -0400

    added costs

--- client.py
+++ client.py
@@ -6,8 +6,9 @@ W_cost = 0
 J_cost = 0
 D_cost = 0
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def init():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    global s
     s.connect(("hackathon.hopto.org", 27832))
     s.send("INIT Midas")
     data = s.recv(1024)
@@ -15,33 +16,45 @@ def init():
     s.send("RECD")
     data = s.recv(1024)
     print data
+    parseCost(data)
+    s.send("START")
+    data = s.recv(1024)
+    print data
 
 def parseCost(data):
-    
-    print "REVENUE "+ Revenue
-    print "WEB " + W_cost
-    print "Java " + J_cost
-    print "Data " + D_cost
+    global Revenue 
+    global W_cost
+    global J_cost
+    global D_cost
+    stuff = data.split()
+    Revenue = int(stuff[1])
+    W_cost = int(stuff[2])
+    J_cost = int(stuff[3])
+    D_cost = int(stuff[4])
+    print "REVENUE "+ str(Revenue)
+    print "WEB " + str(W_cost)
+    print "Java " + str(J_cost)
+    print "Data " + str(D_cost)
 
-s.send("START")
-data = s.recv(1024)
-print data
+
 #while (data != "END"):
+def main():
+    init()
+    for i in xrange(0,5):
+        s.send("RECD")
+        data = s.recv(1024)
+        print data
+        s.send("RECD")
+        data = s.recv(1024)
+        print data
+        s.send("RECD")
+        data = s.recv(1024)
+        print data
+        s.send("CONTROL 0 0 0 0 0 0 0 0 0")
+        data = s.recv(1024)
+        print data
+    s.send("STOP")
 
-for i in xrange(0,5):
-    s.send("RECD")
-    data = s.recv(1024)
-    print data
-    s.send("RECD")
-    data = s.recv(1024)
-    print data
-    s.send("RECD")
-    data = s.recv(1024)
-    print data
-    s.send("CONTROL 0 0 0 0 0 0 0 0 0")
-    data = s.recv(1024)
-    print data
-
-s.send("STOP")
+main()
 
 print "\nENDED"
