d277690c14a318da56aec8a5eacb5484ea1f7cc5
StanleyY <StanleyY@github.com>
2013-05-04 09:28:52 -0400

    commented and second run

--- client.py
+++ client.py
@@ -16,7 +16,9 @@ goingUpJava = {}
 goingUpData = {}
 
 Demand = {}
-Config = {}
+ConfigW = {}
+ConfigJ = {}
+ConfigD = {}
 
 foo = []
 
@@ -26,7 +28,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def init():
     initArrays()
     global s
-    s.connect(("hackathon.hopto.org", 27832))
+    s.connect(("hackathon.hopto.org", 27833))
     s.send("INIT Midas")
     data = s.recv(1024)
     print data
@@ -38,6 +40,7 @@ def init():
     data = s.recv(1024)
     return data
 
+#initalizes the server queue with zeroes
 def initArrays():
     global goingDownJava
     global goingDownData
@@ -81,6 +84,61 @@ def initArrays():
         for j in xrange(0,9):
             goingUpData[i].append(0)
 
+#moves the servers in queue down in one turn
+#those coming online next turn get considered as basically online and vice versa
+def passArrayTime():
+    global goingDownJava
+    global goingDownData
+
+    global goingUpWeb
+    global goingUpJava
+    global goingUpData
+
+    global ConfigW
+    global ConfigJ
+    global ConfigD
+
+    for i in ["NA", "EU", "AP"]:
+        if (goingDownJava[i][0] > 0 ):
+            ConfigJ[i] = ConfigJ[i] - goingDownJava[i][0]
+            goingDownJava[i][0] = 0
+
+        for j in range(1,len(goingDownJava[i])):
+            goingDownJava[i][j-1] = goingDownJava[i][j]
+            goingDownJava[i][j] = 0
+
+        if (goingDownData[i][0] > 0):
+            ConfigD[i] = ConfigD[i] - goingDownData[i][0]
+            goingDownData[i][0] = 0
+
+        for j in range(1,len(goingDownData[i])):
+            goingDownData[i][j-1] = goingDownData[i][j]
+            goingDownData[i][j] = 0
+
+        if (goingUpWeb[i][0] > 0):
+            ConfigW[i] = ConfigW[i] + goingUpWeb[i][0]
+            goingUpWeb[i][0] = 0
+
+        for j in range(1,len(goingUpWeb[i])):
+            goingUpWeb[i][j-1] = goingUpWeb[i][j]
+            goingUpWeb[i][j] = 0
+
+        if (goingUpJava[i][0] > 0):
+            ConfigJ[i] = ConfigJ[i] + goingUpJava[i][0]
+            goingUpJava[i][0] = 0
+
+        for j in range(1,len(goingUpJava[i])):
+            goingUpJava[i][j-1] = goingUpJava[i][j]
+            goingUpJava[i][j] = 0
+
+        if (goingUpData[i][0] > 0):
+            ConfigD[i] = ConfigD[i] + goingUpData[i][0]
+            goingUpData[i][0] = 0
+
+        for j in range(1,len(goingUpData[i])):
+            goingUpData[i][j-1] = goingUpData[i][j]
+            goingUpData[i][j] = 0
+
 #parses cost data and stores it in the respective globals
 def parseCost(data):
     global Revenue 
@@ -98,50 +156,55 @@ def parseCost(data):
     print "Data " + str(D_cost)
     print ""
 
+#calls the logic
 def move():
     global J_cost
     global D_cost
     global Demand
-    global Config
-    currentCapNW = float(Config["W.na"] * 180)
-    currentCapEW = float(Config["W.eu"] * 180)
-    currentCapAW = float(Config["W.ap"] * 180)
-    currentCapNJ = float(Config["J.na"] * 450)
-    currentCapEJ = float(Config["J.eu"] * 450)
-    currentCapAJ = float(Config["J.ap"] * 450)
-    currentCapND = float(Config["D.na"] * 1100)
-    currentCapED = float(Config["D.eu"] * 1100)
-    currentCapAD = float(Config["D.ap"] * 1100)
+    global ConfigW
+    global ConfigJ
+    global ConfigD
+
+    currentCapNW = float(ConfigW["NA"] * 180)
+    currentCapEW = float(ConfigW["EU"] * 180)
+    currentCapAW = float(ConfigW["AP"] * 180)
+    currentCapNJ = float(ConfigJ["NA"] * 450)
+    currentCapEJ = float(ConfigJ["EU"] * 450)
+    currentCapAJ = float(ConfigJ["AP"] * 450)
+    currentCapND = float(ConfigD["NA"] * 1100)
+    currentCapED = float(ConfigD["EU"] * 1100)
+    currentCapAD = float(ConfigD["AP"] * 1100)
 
     projND = calcDemand("NA")
     projED = calcDemand("EU")
     projAD = calcDemand("AP")
 
     control = []
-    #W.na
-    control.append(webLogic(projND, currentCapNW, "W.na"))
-    #W.eu
-    control.append(webLogic(projED, currentCapEW, "W.eu"))
-    #W.ap
-    control.append(webLogic(projAD, currentCapAW, "W.ap"))
-    #J.na
-    control.append(javaLogic(projND, currentCapNJ, "J.na"))
-    #J.eu
-    control.append(javaLogic(projED, currentCapEJ, "J.eu"))
-    #J.ap
-    control.append(javaLogic(projAD, currentCapAJ, "J.ap"))
-    #D.na
-    control.append(dataLogic(projND, currentCapND, "D.na"))
-    #D.eu
-    control.append(dataLogic(projED, currentCapED, "D.eu"))
-    #D.ap
-    control.append(dataLogic(projAD, currentCapAD, "D.ap"))
+    #Web NA
+    control.append(webLogic(projND, currentCapNW, "NA"))
+    #Web EU
+    control.append(webLogic(projED, currentCapEW, "EU"))
+    #Web AP
+    control.append(webLogic(projAD, currentCapAW, "AP"))
+    #Java NA
+    control.append(javaLogic(projND, currentCapNJ, "NA"))
+    #Java EU
+    control.append(javaLogic(projED, currentCapEJ, "EU"))
+    #Java AP
+    control.append(javaLogic(projAD, currentCapAJ, "AP"))
+    #Data NA
+    control.append(dataLogic(projND, currentCapND, "NA"))
+    #Data EU
+    control.append(dataLogic(projED, currentCapED, "EU"))
+    #Data AP
+    control.append(dataLogic(projAD, currentCapAD, "AP"))
 
     val = "CONTROL"
     for i in range(len(control)):
         val += " "
         val += str(control[i])
         #val += str(0)
+    #val = "CONTROL 0 0 0 0 0 0 0 -1 0"
     print val
     return val
 
@@ -205,20 +268,57 @@ def changeDemand(i, trend, region):
         else:
             return current - int(1.5 * dx)
 
+#Decisions on Web Servers
 def webLogic(proj, cap, region):
     global Revenue
     global W_cost
-    return int(math.ceil(proj - cap )/180)
+    global goingUpWeb
+    global goingDownWeb
+    global ConfigW
 
+    val = int(math.ceil(proj - cap)/180)
+    if(ConfigW[region] + val <= 0):
+        return 0
+
+    if (val > 0):
+        goingUpWeb[region][2] = val
+    return val
+
+#Decisions on Java Servers
 def javaLogic(proj, cap, region):
     global Revenue
     global J_cost
-    return int(math.ceil(proj - cap)/450)
+    global goingUpJava
+    global goingDownJava
+    global ConfigJ
 
+    val = int(math.ceil(proj - cap)/450)
+    if (ConfigJ[region] + val <= 0):
+        return 0
+
+    if (val > 0):
+        goingUpJava[region][2] = val
+    else:
+        goingDownJava[region][1] = val
+    return val
+
+#Decisions on Databases
 def dataLogic(proj, cap, region):
     global Revenue
     global D_cost
-    return int(math.ceil(proj - cap)/1100)
+    global goingUpData
+    global goingDownData
+    global ConfigD
+
+    val = int(math.ceil(proj - cap)/1100)
+    if(ConfigD[region] + val <= 0):
+        return 0
+
+    if (val > 0):
+        goingUpData[region][8] = val
+    else:
+        goingDownData[region][2] = val
+    return val
 
 #parses demand data and stores it in global Demand
 #global Demand will later be used to predict future demand
@@ -240,41 +340,50 @@ def parseDemand(data):
        
 #Pretty prints Demand
 def printDemand():
-    demandKeys = sorted(Demand.keys())
-    for i in range (0, len(demandKeys)):
-        print str(demandKeys[i]) + ": " + str(Demand[demandKeys[i]])
+    for i in range (0, len(Demand)):
+        print str(i) + ": { NA: " + str(Demand[i]["NA"]) + " EU: " + str(Demand[i]["EU"]) + " AP: " + str(Demand[i]["AP"]) + " }"
 
     print "length: " + str(len(Demand))
 
 #Stores our current number of servers in each region into Config
 def parseConfig(data):
-    global Config
+    global ConfigW
+    global ConfigJ
+    global ConfigD
     config = data.split()
     config.pop(0)
-    Config["W.na"] = int(config[0])
-    Config["W.eu"] = int(config[1])
-    Config["W.ap"] = int(config[2])
-    Config["J.na"] = int(config[3])
-    Config["J.eu"] = int(config[4])
-    Config["J.ap"] = int(config[5])
-    Config["D.na"] = int(config[6])
-    Config["D.eu"] = int(config[7])
-    Config["D.ap"] = int(config[8])
+    ConfigW["NA"] = int(config[0])
+    ConfigW["EU"] = int(config[1])
+    ConfigW["AP"] = int(config[2])
+    ConfigJ["NA"] = int(config[3])
+    ConfigJ["EU"] = int(config[4])
+    ConfigJ["AP"] = int(config[5])
+    ConfigD["NA"] = int(config[6])
+    ConfigD["EU"] = int(config[7])
+    ConfigD["AP"] = int(config[8])
     
 
 #Pretty prints a key-value pair in Config
 #x is the tier.region you're looking for
 #e.g. printConfig("W.na") prints the number of servers in the Web tier of North America
 #case-sensitive
-def printConfig(x):
-    print x + ": " + Config[x]
+def printConfigW(x):
+    print x + ": " + ConfigW[x]
+
+def printConfigJ(x):
+    print x + ": " + ConfigJ[x]
+
+def printConfigD(x):
+    print x + ": " + ConfigD[x]
 
 #Pretty prints all key-value pairs in Config
 def printAllConfig():
-    configKeys = sorted(Config.keys())
-    configKeys.reverse()
-    for i in range(0,len(configKeys),3):
-        print str(configKeys[i]) + ": " + str(Config[configKeys[i]]) + " \t" + str(configKeys[i+1]) + ": " + str(Config[configKeys[i+1]]) + " \t" + str(configKeys[i+2]) + ": " + str(Config[configKeys[i+2]])
+    global ConfigW
+    global ConfigJ
+    global ConfigD
+    
+    for i in ["NA", "EU", "AP"]:
+        print i + " Web: " + str(ConfigW[i]) + " \t" + i + " Java: " + str(ConfigJ[i]) + " \t" + i + " Data: " + str(ConfigD[i])
 
 def main():
     data = init()
@@ -290,7 +399,7 @@ def main():
         data = s.recv(1024)
         print data
         parseDemand(data)
-        printDemand()
+        #printDemand()
         s.send("RECD")
         #DIST
         data = s.recv(1024)
@@ -299,11 +408,12 @@ def main():
         #PROFIT
         data = s.recv(1024)
         print data
+        passArrayTime()
         s.send(move())
         print ""
         #CONFIG
         data = s.recv(1024)
-        print data
+        #print data
         if(endnum <= 2880 and i > endnum):
             endnum = i - 1 + int(raw_input("Run how many turns more? Enter 2880 to run til end\n"))
             print "CURRENT ENDNUM"
