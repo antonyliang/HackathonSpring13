92422b3e0590e6cf3d94c83d57f3783771c8e9fa
StanleyY <StanleyY@github.com>
2013-05-03 20:27:09 -0400

    basic socket

--- client.py
+++ client.py
@@ -3,7 +3,9 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("hackathon.hopto.org", 27832))
-s.send("Hello")
-data = s.recv(1024)
+s.send("INIT {Team Midas}")
+data = s.recv(2048)
+
+print data
 
 print "lol"
