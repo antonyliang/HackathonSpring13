af243e4f4d0276b3a67d1b7f7bd39b82cde64f74
Andrew Spano <andrew.spano1@marist.edu>
2013-05-04 10:30:05 -0400

    deleted contents, starting from scratch

--- gui/fileio.js
+++
@@ -1,9 +0,0 @@
-//jquery ajax call to open data file
-var file = null;
-function read_file() {
-$.ajax({url: "data.txt", success: function(data) {
-    alert("THIS IS THE DATA MAN " + data);
-    file = data;
-}});
-alert("FILE MAN FILE " + file);
-return file;}
